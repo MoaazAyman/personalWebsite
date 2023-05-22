375d1cf8de7ecadc8568e1240c4a7c7b1fc60522
Moaaz <moaazayman71@gmail.com>
2023-05-22 21:16:27 +0300

    first commit

---
+++ Javascript/loginForm/main.js
@@ -0,0 +1,28 @@
+let wrapper = document.querySelector('.wrapper');
+let registerLink = document.querySelector('.register-link');
+let loginLink = document.querySelector('.login-link');
+let logiBtn = document.querySelector('.btnLogin-popup');
+let closeIcon = document.querySelector('.icon-close');
+let slideShow = document.querySelector('.slide-show');
+
+
+
+registerLink.addEventListener('click', ()=> {
+    wrapper.classList.add('active');
+})
+
+loginLink.addEventListener('click', ()=> {
+    wrapper.classList.remove('active');
+})
+
+
+logiBtn.addEventListener('click', () => {
+    wrapper.classList.add('reactive');
+})
+
+closeIcon.addEventListener('click', () => {
+    wrapper.classList.remove('reactive');
+    wrapper.classList.remove('active');
+})
+
+
